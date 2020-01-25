6b7da919af776a3fac0b1824c4c4add6eb13e2be
Hossein <hdaneshfar@bipc.org.ir>
2020-01-25 10:26:25 +0330

    Add project files.

---
+++ AspCoreExtend/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspCoreExtend
+{
+    public static class calculator
+    {
+        public static int Sum(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
